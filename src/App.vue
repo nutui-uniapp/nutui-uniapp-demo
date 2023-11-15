205ee906d79c89d84426f1ca2de3a93145538d10
yang1206 <yang15188280673@outlook.com>
2023-11-15 10:51:49 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { onHide, onLaunch, onShow } from '@dcloudio/uni-app'
+
+onLaunch(() => {
+  console.log('App Launch')
+})
+onShow(() => {
+  console.log('App Show')
+})
+onHide(() => {
+  console.log('App Hide')
+})
+</script>
+
+<style lang="scss">
+@import 'nutui-uniapp/styles/index';
+</style>
